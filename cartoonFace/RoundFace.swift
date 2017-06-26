4dd4dab83de75f9449a25c08699c055fa1f9b84d
JiaYanwei <Jovi@jia-3.local>
2017-06-26 11:33:48 +0800

    Transfer to git version control.building a git configuration

--- cartoonFace/RoundFace.swift
+++ cartoonFace/RoundFace.swift
@@ -8,14 +8,57 @@
 
 import UIKit
 
+
+@IBDesignable
 class RoundFace: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable
+    var emotionFactor: Double = 1.0
+    
+    @IBInspectable
+    var faceColor: UIColor = UIColor.blue
+    
+    @IBInspectable
+    var scaleFactor: Double = 0.9
+    
+    @IBInspectable
+    var lineWidth: Double = 5.0
+    
+    
+    enum sideOfEye {
+        case left
+        case right
     }
-    */
-
+    
+    struct ratioOfEyeToHead {
+        static let eyeCenterToHead: Double = 3
+        static let eyeOffsetToHead: Double = 3
+        static let eyeRadiousToHead: Double = 0.4
+        
+    }
+    
+    private var centerOfHead: CGPoint {
+        return CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+    }
+    
+    private var faceRadius: CGFloat {
+        return CGFloat(min(self.bounds.maxX, self.bounds.maxY) / 2 * CGFloat(scaleFactor))
+    }
+    
+    private func faceBezierPath() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: centerOfHead, radius: faceRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: false)
+        
+        path.lineWidth = CGFloat(lineWidth)
+        return path
+        
+    }
+    
+    private func eyeBezierpath(eye: sideOfEye) -> UIBezierPath {
+        func eyeCenter(side: sideOfEye)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        faceColor.set()
+        faceBezierPath().stroke()
+    }
+    
 }

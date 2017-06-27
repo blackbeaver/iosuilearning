09776b90b9f3fe52dfa858eaaf187d2de3a0129e
JiaYanwei <Jovi@jia-3.local>
2017-06-27 17:29:47 +0800

    eye & mouth are done

--- cartoonFace/RoundFace.swift
+++ cartoonFace/RoundFace.swift
@@ -18,10 +18,10 @@ class RoundFace: UIView {
     var faceColor: UIColor = UIColor.blue
     
     @IBInspectable
-    var scaleFactor: Double = 0.9
+    var scaleFactor: CGFloat = 0.9
     
     @IBInspectable
-    var lineWidth: Double = 5.0
+    var lineWidth: CGFloat = 5.0
     
     
     enum sideOfEye {
@@ -34,8 +34,11 @@ class RoundFace: UIView {
         static let eyeYOffsetToHeadRadius: CGFloat = 3
         static let eyeRadiousToHeadRadius: CGFloat = 0.2
         
-        static let mouth1ToHeadRadius: CGFloat = 3
-        static
+        static let mouthYOffsetToHeadRadius: CGFloat = 3
+        static let mouthMidXOffsetToHeadRadius: CGFloat = 0.25
+        static let mouthMaxXOffsetToHeadRadius: CGFloat = 0.5
+        static let mouthHeightToHeadRadius: CGFloat = 3
+        
     }
     
     private var centerOfHead: CGPoint {
@@ -47,26 +50,74 @@ class RoundFace: UIView {
     }
     
     private func faceBezierPath() -> UIBezierPath {
-        let path = UIBezierPath(arcCenter: centerOfHead, radius: faceRadius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: false)
+        let path = UIBezierPath(arcCenter: centerOfHead,
+                                radius: faceRadius,
+                                startAngle: 0,
+                                endAngle: CGFloat.pi * 2,
+                                clockwise: false)
         
-        path.lineWidth = CGFloat(lineWidth)
+        path.lineWidth = lineWidth
         return path
         
     }
     
     private func eyeBezierpath(eye: sideOfEye) -> UIBezierPath {
+        let eyeY = centerOfHead.y - faceRadius / ratioOfEyeToHead.eyeYOffsetToHeadRadius
+        let eyeRadius = faceRadius * ratioOfEyeToHead.eyeRadiousToHeadRadius
+        
         func eyeCenter(side: sideOfEye) -> CGPoint {
-        }
-            let x = centerOfHead.y - faceRadius / ratioOfEyeToHead.eyeYOffsetToHeadRadius
-            let y = centerOfHead.x + ((eye == .left) ? -1 : 1) * faceRadius * ratioOfEyeToHead.eyeXOffsetToHeadRadius
+            let x = centerOfHead.x + ((eye == .left) ? -1 : 1) * faceRadius * ratioOfEyeToHead.eyeXOffsetToHeadRadius
             
-            return CGPoint(x: x, y: y)
+            return CGPoint(x: x, y: eyeY)
         }
+
+        
+        let path = UIBezierPath(arcCenter: eyeCenter(side: eye),
+                                radius: eyeRadius,
+                                startAngle: 0,
+                                endAngle: CGFloat.pi * 2,
+                                clockwise: true)
+        path.lineWidth = lineWidth
+/*
+        let path = UIBezierPath()
+        let eyeSPX = eyeCenter(side: eye).x - eyeRadius
+        let eyeEPX = eyeCenter(side: eye).x + eyeRadius
+        path.move(to: CGPoint(x: eyeSPX, y: eyeY))
+        path.addLine(to: CGPoint(x: eyeEPX, y: eyeY))
+ */
+        
+        return path
+    }
+    
+    private func mouthBezierPath() -> UIBezierPath {
+        let mouthHeight = faceRadius / ratioOfEyeToHead.mouthHeightToHeadRadius
+        let mouthY = centerOfHead.y + faceRadius / ratioOfEyeToHead.mouthYOffsetToHeadRadius
+        
+        
+        
+        let mouthP1X = centerOfHead.x - faceRadius * ratioOfEyeToHead.mouthMaxXOffsetToHeadRadius
+        let mouthP2X = centerOfHead.x - faceRadius * ratioOfEyeToHead.mouthMidXOffsetToHeadRadius
+        let mouthP3X = centerOfHead.x + faceRadius * ratioOfEyeToHead.mouthMidXOffsetToHeadRadius
+        let mouthP4X = centerOfHead.x + faceRadius * ratioOfEyeToHead.mouthMaxXOffsetToHeadRadius
+        
+        let mouthP2P3Y = mouthY + CGFloat(max(-1, min(emotionFactor, 1))) * mouthHeight
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: mouthP1X, y: mouthY))
+        path.addCurve(to: CGPoint(x: mouthP4X, y: mouthY),
+                      controlPoint1: CGPoint(x: mouthP2X, y: mouthP2P3Y),
+                      controlPoint2: CGPoint(x: mouthP3X, y: mouthP2P3Y))
+        path.lineWidth = lineWidth
+        
+        return path
     }
     
     override func draw(_ rect: CGRect) {
         faceColor.set()
         faceBezierPath().stroke()
+        eyeBezierpath(eye: .left).stroke()
+        eyeBezierpath(eye: .right).stroke()
+        mouthBezierPath().stroke()
     }
     
 }
